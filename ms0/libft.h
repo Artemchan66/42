fbe54f55c6906ed100e484b099be764408164d14
Artem Abdullaev <arabdull@c3r8s3.42warsaw.pl>
2025-09-25 20:55:10 +0200

    init

---
+++ ms0/libft.h
@@ -0,0 +1,7 @@
+#ifndef libft_h
+
+#define libft_h
+
+int ft_isalpha(int c);
+
+#endif
---
+++ ms0/test.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "libft.h"
+
+int main()
+{
+	printf("%d", ft_isalpha('a'));
+	return 0;
+}
