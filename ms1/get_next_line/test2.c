22e2e3c432225d777c24f2f5a0345cc69400db50
Artemchan66 <artemchan@gmail.com>
2025-12-27 14:33:22 +0100

    shit is actually working

--- ms1/get_next_line/test2.c
+++ ms1/get_next_line/test2.c
@@ -1,5 +1,5 @@
 #ifndef BUFFER_SIZE
-#define BUFFER_SIZE 1024
+#define BUFFER_SIZE 42
 #endif
 
 #include <unistd.h> //read();
@@ -95,7 +95,8 @@ char	*get_next_line(int fd)
 {
 	//string for reading data from fd
 	//will be freed when we'll find newline
-	char	reading_buffer[BUFFER_SIZE];	
+	//add +1 to null terminate
+	char	reading_buffer[BUFFER_SIZE + 1];
 	//static pointer to string in which we will store everything we read from fd
 	static char *character_storage;
 	//pointer to string that we want to output
@@ -106,38 +107,76 @@ char	*get_next_line(int fd)
 	int		i;
 	//if newline found - 1, not - 0
 	int		is_newline_found;
-
-	printf("%d", 1);
-	//populating reading_buffer with character from fd
-	bytes_from_read = read(fd, reading_buffer, BUFFER_SIZE);
-	//file is empty - returning NULL
-	if (bytes_from_read == 0)
-		return NULL;
-
-	bytes_from_read = 1;
-
-	printf("%d", 1);
+	//line print iterator
+	int	j;
 
 	//reading all data from fd into static character_storage
 	//if we find a new line, mark it and wirte the remaining buffer_size values
 	is_newline_found = 0;
 	while(is_newline_found == 0)
 	{
+		//populating reading_buffer with character from fd
+		bytes_from_read = read(fd, reading_buffer, BUFFER_SIZE);
+		//reading error - returning NULL
+		if (bytes_from_read < 0)
+			return NULL;
+
+		if (bytes_from_read == 0)
+		{
+			if (!character_storage || character_storage[0] == '\0')
+				return NULL;
+			else
+				break;
+		}
+
+		//null terminating to avoid shitty leaks
+		reading_buffer[bytes_from_read] = '\0';
 		character_storage = ft_strjoin(character_storage, reading_buffer);
 
 		if (ft_strchr(reading_buffer, '\n'))
 			is_newline_found = 1;
-		else
-		{	
-			bytes_from_read = read(fd, reading_buffer, BUFFER_SIZE);
-		}
 	}
 	i = 0;
-	while (character_storage[i] != '\n')
-	{
-		output_line[i] = character_storage[i];
+	while (character_storage[i] != '\0' && character_storage[i] != '\n')
 		i++;
+
+	if (character_storage[i] == '\n')
+		i++;
+
+	//+1 for null
+	output_line = malloc(i + 1);
+	if (!output_line)
+		return NULL;
+
+	j = 0;
+	while (j < i)
+	{
+		output_line[j] = character_storage[j];
+		j++;
 	}
+	output_line[j++] = '\0';
+
+	//printf("%s\n\n", output_line);
+	//printf("%s\n\n", character_storage);
+
+	//finding size of remainig lenght by moving a pointer by the size of the line we printed;
+	size_t remaining_len = ft_strlen(character_storage + i);
+
+	//getting memory for new buffer without printed line;
+	char *buff_character_storage = malloc(remaining_len + 1);
+	if (!buff_character_storage)
+		return NULL;
+
+	//copying from storage to buffer
+	ft_memcpy(buff_character_storage, character_storage + i, remaining_len);
+
+	buff_character_storage[remaining_len] = '\0';
+	//disposing storage
+	free(character_storage);
+	//populating updated data
+	character_storage = buff_character_storage;
+
+	//printf("%s", character_storage);
 
 	return output_line;
 }
@@ -145,7 +184,11 @@ char	*get_next_line(int fd)
 int	main()
 {
 	int fd = open("aboba.txt", O_RDONLY);
+	
+	//get_next_line(fd);
 
+	printf("%s", get_next_line(fd));
+	printf("%s", get_next_line(fd));
 	printf("%s", get_next_line(fd));
 }
 
