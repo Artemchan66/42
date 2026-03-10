d572dc4edae8f21b58cadd87fce77a02fd17b275
Artemchan66 <artemchan@gmail.com>
2026-03-10 17:51:41 +0100

    wip

--- ms2/push_swap/src/push_swap.c
+++ ms2/push_swap/src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: arabdull <arabdull@student.42warsaw.pl>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/05 16:35:14 by arabdull          #+#    #+#             */
-/*   Updated: 2026/03/05 18:55:42 by arabdull         ###   ########.fr       */
+/*   Updated: 2026/03/10 17:44:17 by arabdull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,12 @@ char** push_swap(int a[])
 	return out;
 }
 
+void	pa(t_stack stack, int val)
+{
+
+}
+
+//bubble sort
 void sort(int arr[], int n)
 {
 	int	swapped;
@@ -67,6 +73,11 @@ t_stack *init_stack(int capacity)
 	stack->size = 0;
 	stack->capacity = capacity;
 	return stack;
+}
+
+int	get_bit(int val, int bit_index)
+{
+	return ((val >> bit_index) & 1);
 }
 
 int	main(int argc, char *argv[])
@@ -119,7 +130,38 @@ int	main(int argc, char *argv[])
 		printf("%d ", sorted_indexed_arr[i]);
 	printf("\n");
 
-	
+	int a = 2;
+	int b = a << 1;
+	printf("%d\n", b);
+
+	int maxVal = 0;
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > maxVal)
+			maxVal = arr[i];
+	}
+
+	printf("%d\n", maxVal);
+
+	int maxBits = 0;
+	while (maxVal > 0)
+	{
+		maxVal >>= 1;
+		maxBits++;
+	}
+
+	for (int i = 0; i < maxBits; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (get_bit(arr[j], i))
+			{
+				
+			}
+		}
+	}
+
+	printf("%d\n", get_bit(2, 1));
 
 	free(stack_a->data);
 	free(stack_a);
